5e3b83315e98619b6f6c83319c41cdd9c05e2f11
maxim <max.zhcrazy@gmail.com>
2024-06-15 19:30:51 +0200

    1) Have done 2.1 and 2.2 HW 07.06.24  - Changed simple variables to more informative variables  - Made f-line in 'print()'
     2) Have done 3.1 HW 15.06.24

---
+++ HomeWork3.1.py
@@ -0,0 +1,14 @@
+num_1 = int(input('Enter your First number: '))
+num_2 = int(input('Enter your Second number: '))
+operation = input('Enter operation you want to do (+; -; *; /): ')
+
+if operation == '+':
+    print(f'{num_1} + {num_2} =', num_1 + num_2)
+elif operation == '-':
+    print(f'{num_1} - {num_2} =', num_1 - num_2)
+elif operation == '*':
+    print(f'{num_1} * {num_2} =', num_1 * num_2)
+elif operation == '/':
+    print(f'{num_1} / {num_2} =', num_1 / num_2)
+else:
+    print('You chose wrong operation! Try again!')
