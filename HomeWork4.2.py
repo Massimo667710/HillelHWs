282ddf28d7f66b6df41b71b316f3e526aa93a58b
maxim <max.zhcrazy@gmail.com>
2024-06-22 19:39:39 +0200

    1) Have done 4.2 HW 22.06.24

--- HomeWork4.2.py
+++ HomeWork4.2.py
@@ -3,6 +3,5 @@ list_of_numbers = [1, 3, 5]
 if len(list_of_numbers) == 0:
     print(f'{list_of_numbers}')
 else:
-    last_number = list_of_numbers[-1]
     result = sum(list_of_numbers[number] for number in range(0, len(list_of_numbers), 2))
     print(f'{list_of_numbers} =>', result * list_of_numbers[-1])
