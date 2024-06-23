58305952bb327b51debab5fb43ce5ad4d1e5f5dd
maxim <max.zhcrazy@gmail.com>
2024-06-23 13:15:06 +0200

    1) Have done 4.3 HW 23.06.24

---
+++ HomeWork4.3.py
@@ -0,0 +1,8 @@
+import random
+
+random_list = [random.randint(1, 10) for numbers in range(random.randint(3, 10))]
+
+new_list = [random_list[0], random_list[2], random_list[-2]]
+
+print(f'{random_list} => ', new_list)
+
