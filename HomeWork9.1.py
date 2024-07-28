1512b50d6d2f5462f099e37b2f9d1a2d190c6aec
maxim <max.zhcrazy@gmail.com>
2024-07-28 12:06:16 +0200

    1) Have done 9.1 HW 28.07.24

---
+++ HomeWork9.1.py
@@ -0,0 +1,14 @@
+def popular_words(text, words):
+    text_words = text.lower().split()
+
+    popular_dict_words = {}
+
+    for word in words:
+        popular_dict_words[word] = text_words.count(word)
+
+    return popular_dict_words
+
+
+assert popular_words('''When I was One I had just begun When I was Two I was nearly new ''',
+                     ['i', 'was', 'three', 'near']) == {'i': 4, 'was': 3, 'three': 0, 'near': 0}, 'Test1'
+print('OK')
