38b158a7e44be318366b922d2b444de04dd7d384
maxim <max.zhcrazy@gmail.com>
2024-07-28 12:55:12 +0200

    1) Have done 11.3 HW 28.07.24

---
+++ HomeWork11.3.py
@@ -0,0 +1,16 @@
+def is_even(number):
+    list_num = list(str(number))
+    num_list = ('0', '2', '4', '6', '8')
+
+    if list_num[-1] in num_list:
+        result = True
+    else:
+        result = False
+
+    return result
+
+
+assert is_even(2494563894038**2) == True, 'Test1'
+assert is_even(1056897**2) == False, 'Test2'
+assert is_even(24945638940387**3) == False, 'Test3'
+print('Ok')
