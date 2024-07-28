9fe628d9a5d011a9873071c9a2a62de27cf7583f
maxim <max.zhcrazy@gmail.com>
2024-07-28 21:46:58 +0200

    1) Have done 11_3 28.07.24

--- 11_3.py
+++ 11_3.py
@@ -1,13 +1,18 @@
 def is_even(number):
     list_num = list(str(number))
-    num_list = ('0', '2', '4', '6', '8')
 
-    if list_num[-1] in num_list:
-        result = True
-    else:
-        result = False
+    # 1st Option
+    # num_list = ('0', '2', '4', '6', '8')
 
-    return result
+    # if list_num[-1] in num_list:
+    #     result = True
+    # else:
+    #     result = False
+
+    # return result
+
+    # 2nd Option
+    return list_num[-1] in ('0', '2', '4', '6', '8')
 
 
 assert is_even(2494563894038**2) == True, 'Test1'
