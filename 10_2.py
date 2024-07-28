21fa45298cf56a0743944be98b31748379743486
maxim <max.zhcrazy@gmail.com>
2024-07-28 13:50:48 +0200

    1) Have done 10_2 28.07.24

---
+++ 10_2.py
@@ -0,0 +1,15 @@
+def first_word(text):
+    cleaned_text = ''.join([char if char.isalnum() or char in "' " else ' ' for char in text])
+
+    words = cleaned_text.split()
+
+    return words[0]
+
+
+assert first_word("Hello world") == "Hello", 'Test1'
+assert first_word("greetings, friends") == "greetings", 'Test2'
+assert first_word("don't touch it") == "don't", 'Test3'
+assert first_word(".., and so on ...") == "and", 'Test4'
+assert first_word("hi") == "hi", 'Test5'
+assert first_word("Hello.World") == "Hello", 'Test6'
+print('OK')
